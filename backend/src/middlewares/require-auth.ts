6a9f280a9a914cbbd7e734537f2914084b00fee7
Matt Fu <matthewfu21@gmail.com>
2024-04-25 02:34:12 -0400

    Remove debugging statements

--- backend/src/middlewares/require-auth.ts
+++ backend/src/middlewares/require-auth.ts
@@ -1,5 +1,4 @@
 const requireAuth = (req, res, next) => {
-  console.log('Checking authentication for', req.method, req.path);
   if (!req.session!.user) {
     next(new Error('Cannot logout when not logged in.'));
   } else {
