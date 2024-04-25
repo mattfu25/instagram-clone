de97e0b19688fb5bcadbe16842113931f2d3308d
Matt Fu <matthewfu21@gmail.com>
2024-04-25 02:33:27 -0400

    Update style

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,4 +1,4 @@
-// Configurations 
+// Configurations
 import { PrismaClient } from '@prisma/client';
 import AWS from 'aws-sdk';
 
@@ -7,11 +7,11 @@ const prisma = new PrismaClient();
 
 // S3
 const s3 = new AWS.S3({
-    accessKeyId: process.env.ACCESS_KEY,
-    secretAccessKey: process.env.SECRET_KEY,
-    endpoint: process.env.ENDPOINT, 
-    s3ForcePathStyle: true, 
-    signatureVersion: 'v4'
+  accessKeyId: process.env.ACCESS_KEY,
+  secretAccessKey: process.env.SECRET_KEY,
+  endpoint: process.env.ENDPOINT,
+  s3ForcePathStyle: true,
+  signatureVersion: 'v4',
 });
 
 export { prisma, s3 };
--- backend/src/middlewares/require-auth.ts
+++ backend/src/middlewares/require-auth.ts
@@ -1,10 +1,10 @@
 const requireAuth = (req, res, next) => {
-    console.log("Checking authentication for", req.method, req.path);
-    if (!req.session!.user) {
-      next(new Error('Cannot logout when not logged in.'));
-    } else {
-      next();
-    }
-  };
-  
-  export default requireAuth;
+  console.log('Checking authentication for', req.method, req.path);
+  if (!req.session!.user) {
+    next(new Error('Cannot logout when not logged in.'));
+  } else {
+    next();
+  }
+};
+
+export default requireAuth;
