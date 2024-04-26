53e75bb73ff1c592240ccdfb0896cdac99ae8395
Matt Fu <matthewfu21@gmail.com>
2024-04-26 00:54:06 -0400

    Clean up minio and aws sdk dependencies, configs, and gitignore

--- backend/src/config.ts
+++ backend/src/config.ts
@@ -1,17 +1,7 @@
 // Configurations
 import { PrismaClient } from '@prisma/client';
-import AWS from 'aws-sdk';
 
 // Prisma
 const prisma = new PrismaClient();
 
-// S3
-const s3 = new AWS.S3({
-  accessKeyId: process.env.ACCESS_KEY,
-  secretAccessKey: process.env.SECRET_KEY,
-  endpoint: process.env.ENDPOINT,
-  s3ForcePathStyle: true,
-  signatureVersion: 'v4',
-});
-
-export { prisma, s3 };
+export { prisma };
