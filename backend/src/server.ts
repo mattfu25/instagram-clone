f83c73215cf08345127959a99074c1133f3916c4
Matt Fu <matthewfu21@gmail.com>
2024-08-01 13:56:11 -0400

    Add initial cors handling to server (wildcard)

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import cors from 'cors';
 import express from 'express';
 import cookieSession from 'cookie-session';
 
@@ -16,6 +17,7 @@ const PORT = process.env.PORT;
 const app = express();
 
 // Middleware
+app.use(cors());
 app.use(express.json());
 app.use(
   cookieSession({
